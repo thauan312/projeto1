4732e600b249cc6e9c0a1e964ec0f7d75600c3ab
miranda <joliveiramiranda30@gmail.com>
2021-04-28 22:08:36 -0300

    Nome do app

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,7 @@ const MainNavigator = createStackNavigator({
     Feed
 },{
     defaultNavigationOptions: {
-        headerTitle: <Text>RECLAME GUANAMBI</Text>,
+        headerTitle: <Text>FISCALIZE GUANAMBI</Text>,
         headerLeft: (
             <TouchableOpacity style={{ marginLeft: 20, }}>
                 <Image source={camera} />
